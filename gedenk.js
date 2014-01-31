07a76ea27c3c5fef4a7fcc5f423ececaaf521ef6
Vic Mortelmans <vic.mortelmans@telenet.be>
2014-01-31 11:54:38 -0500

    committing to show a problem with sliders

---
+++ gedenk.js
@@ -0,0 +1,17 @@
+var Webflow = Webflow || [];  
+Webflow.push(function () { 
+    $('h1').on('click', function() {
+        if ( $(this).parent().find('.slider').is(':visible') ) {
+            $('.slider').slideUp();
+        } else {
+            $('.slider').hide();
+            $(this).parent().find('.slider').slideDown();
+            $('html, body').animate({
+                scrollTop: ($(this).offset().top)
+            },500);
+        }
+    });
+    setTimeout(function() {
+        $('.slider').hide()
+    }, 0);
+});
